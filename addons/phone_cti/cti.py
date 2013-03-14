a6db6800b2f414576bc8d63f3d13517306b8319f
Christophe CHAUVET <christophe.chauvet@syleam.fr>
2013-03-14 15:09:37 +0100

    [ADD] Add cti.action field on company, to return action by defaut [ADD] Add function to custom how to retrieve partner and address since the phone number [IMP] Improve incoming function

--- addons/phone_cti/cti.py
+++ addons/phone_cti/cti.py
@@ -27,6 +27,7 @@ from openerp.osv import fields
 import uuid
 
 
+# You can inherit the cti.url_config in your profile if you want a custom documentation in your language
 _default_parameters = {
     "cti.uuid": lambda: str(uuid.uuid1()),
     "cti.url_config": lambda: 'http://syleam.github.com/phone_cti/configuration.html',
@@ -41,6 +42,8 @@ class cti_action(osv.osv):
         'name': fields.char('Name', size=64, help='Name of this action'),
         'model_id': fields.many2one('ir.model', 'Model', help='Model to launch search or name_search'),
         'field_name': fields.char('Field name', size=32, help='Field name to pass on search to find records'),
+        'act_window_id': fields.many2one('ir.actions.act_window', 'Act Window', help='Select act windows'),
+
     }
 
     def init(self, cr):
@@ -50,7 +53,7 @@ class cti_action(osv.osv):
         """
         param_obj = self.pool.get('ir.config_parameter')
         for key, func in _default_parameters.iteritems():
-            ids = param_obj.search(cr, 1, [('key','=',key)])
+            ids = param_obj.search(cr, 1, [('key', '=', key)])
             if not ids:
                 param_obj.set_param(cr, 1, key, func())
 
@@ -63,6 +66,61 @@ class cti_action(osv.osv):
         token = ir_config_obj.get_param(cr, 1, 'cti.uuid')
         return '%s/cti/incoming?database=%s&user=%s&token=%s&phone=%s' % (url, cr.dbname, 'admin', token, 'XXX')
 
+    def inspect_incoming(self, cr, uid, phone_number, context=None):
+        """
+        This function, check the phone number, and the user
+        """
+        # Retrieve the default URL for the web client when no action found
+        ir_config_obj = self.pool.get('ir.config_parameter')
+        url = ir_config_obj.get_param(cr, 1, 'web.base.url', 'http://localhost:8069')
+
+        # Retrieve the context for the user
+        user_context = self.pool.get('res.users').context_get(cr, uid, context=context)
+        if context is not None:
+            user_context.update(context)
+
+        # Search address and partner for this phone number
+        (partner_id, address_id) = self.find_partner_from_phone_number(cr, uid, phone_number, context=user_context)
+        if not partner_id and not address_id:
+            return url
+
+
+
+        return url
+
+    def find_partner_from_phone_number(self, cr, uid, phone_number, context=None):
+        """
+        You can inherit this function, if you change the storage format for phone number in your database
+        """
+        if context is None:
+            context = self.pool.get('res.users').context_get(cr, uid, context=context)
+
+        search_args = [
+            '|',
+            ('phone', '=', phone_number),
+            ('mobile', '=', phone_number),
+        ]
+        address_obj = self.pool.get('res.partner.address')
+        address_ids = address_obj.search(cr, uid, search_args, context=context)
+        if not address_ids:
+            return False, False
+
+        address_id = address_ids[0]
+        partner_id = address_obj.browse(cr, uid, address_id, context=context).partner_id
+        partner_id = partner_id and partner_id.id or False
+
+        return partner_id, address_id
+
 cti_action()
+
+
+class res_company(osv.osv):
+    _inherit = 'res.company'
+
+    _columns = {
+        'cti_action_id': fields.many2one('cti.action', 'CTI Action', help='CTI action by default when no match found'),
+    }
+
+res_company()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
